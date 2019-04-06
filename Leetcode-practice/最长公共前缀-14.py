56578bb91871a35c9bff6e7c1c356e258a160a2d
yjfiejd <yjfiejd@163.com>
2019-04-06 15:53:35 +0800

    add 有效括号

--- Leetcode-practice/最长公共前缀-14.py
+++ Leetcode-practice/最长公共前缀-14.py
@@ -2,25 +2,66 @@
 # @File : 最长公共前缀-14.py
 
 
-a = ["flower", "flow", "flight"]
+# class Solution:
+#
+#     def longestCommonPrefix(self, strs):
+#
+#         result = ""
+#         i = 0
+#
+#         while True:
+#             try:
+#                 sets = set(string[i] for string in strs)
+#                 if len(sets) == 1:
+#                     result += sets.pop()
+#                     i += 1
+#                 else: break
+#             except Exception as e:
+#                 break
+#         return result
 
-def longestCommonPrefix(strs):
-    """
-    :param strs:
-    :return: 最长公共前缀
-    """
+class Solution:
 
-    if not strs:
-        return ""
-    s1 = min(strs)
-    s2 = max(strs)
+    def longestCommonPrefix(self, strs):
 
-    for i, x in enumerate(s1):
-        if x != s2[i]:
-            return s2[:i]
-    return s1
+        if len(strs) < 1 or not strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+
+         # 元素全部相同
+        if len(set(strs)) == 1:
+            return strs[0]
+
+        # 找最短str
+        short_i = 0
+        temp_len = len(strs[0])
+        for i in range(1, len(strs)):
+            if len(strs[i]) < temp_len:
+                short_i = i
+
+        # 找最短前缀
+        short_str = strs[short_i]
+        for i in range(len(short_str)):
+            for string in strs:
+                # print(string[i])
+                # print(short_str[i])
+                if string[i] != short_str[i]:
+                    return short_str[:i]
+
+        return short_str
 
 
+        # for j in range(1, len(strs)):
+        #     while (strs[j].index(short_str) != 0):
+        #         short_str = short_str[:len(short_str) - 1]
+        #         print(short_str)
+        # return short_str
 
-b = longestCommonPrefix(a)
-print(b)
+
+a = Solution()
+# rel = a.longestCommonPrefix(["abab","aba","abc"])
+# rel = a.longestCommonPrefix(["ca","a"])
+# rel = a.longestCommonPrefix(["c","c"])
+rel = a.longestCommonPrefix(["aa","a"])
+print(rel)
--- Leetcode-practice/有效的括号.py
+++ Leetcode-practice/有效的括号.py
@@ -1,22 +1,51 @@
 # @TIME : 2019/4/2 上午8:36
 # @File : 有效的括号.py
 
-
-class Solution:
-    def isValid(self, s: str) -> bool:
-        ok_dict = {
-            "1": ["(", ")"],
-            "2": ["{", "}"],
-            "3": ["[", "]"]
-        }
-
-        # 从左到有，搜索，第一个属于哪个集合
+import time
 
 
-a = Solution()
-a.isValid("(){}")
+class Solution1:
+
+    def isValid(self, s):
+
+        stack = []
+        dict1 = {"(": ")", "{": "}", "[": "]"}
+
+        for elem in s:
+            if elem in dict1.keys():
+                stack.append(elem)
+            elif elem in dict1.values():
+                if stack == [] or dict1[stack.pop()] != elem:
+                    return False
+            else:
+                return False
+        return stack == []
 
 
+class Solution2:
+    def isValid(self, s):
+        while '{}' in s or '()' in s or '[]' in s:
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+            s = s.replace('()', '')
+        return s == ''
 
 
-lxc2530366
+input_1 = "()[{}]"
+input_2 = "{[]]}"
+
+start1 = time.time()
+aa = Solution1()
+end1 = time.time()
+time1 = end1 - start1
+# print(aa.isValid(input_1))
+print('第一个程序运行时间：', str(time1))
+
+start2 = time.time()
+bb = Solution2()
+end2 = time.time()
+time2 = end2 - start2
+# print(bb.isValid(input_1))
+print('第二个程序运行时间：', str(time2))
+
+print("第一个程序时间减去第二个程序时间 = ", str(time1 - time2))
